df9f10e80d75f7093bb0c73929c78be2c76359e5
RUSHIL SHARMA <221b313@juetguna.in>
2025-01-22 15:19:10 +0530

    Update Application1.java

--- Application1.java
+++ Application1.java
@@ -1,22 +1,3 @@
-/*
-1. Test for Inheritance
-Make a class Mother with int x and void show ( ) as its members. Now create another 
-class Child that is derived from the Mother class. Create another class Application to 
-test the inheritance. 
-class Mother {.......}
-class Child extends Mother {// Empty Body } Note: “//” is used for single line comments 
- in java.
-class Application {
- public static void main (String args[]){
- Mother m= new Mother ( );
- m.show( ); // show of Mother is called
- Child ch=new Child ( ); 
- ch. show ( ); // show ( ) inherited in Child from Mother is called
- }
-*/
-
-
-
 class Mother
 {
     int x;
