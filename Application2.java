3d5907cdffa7d71989fae7dffd85f9bd4fd64d3d
RUSHIL SHARMA <221b313@juetguna.in>
2025-01-22 15:17:18 +0530

    Update Application2.java

--- Application2.java
+++ Application2.java
@@ -1,13 +1,3 @@
-/*
-2. Test for Overriding
-Make a function with the same return type, name of the function, number and type of 
-arguments in the Child class as they are in the Mother class. Change the string to be 
-displayed on the screen. For example, if Mother class version of show ( ) was displaying 
-“Hello World” then the child class version of show ( ) will display “Hello JUET”. Run the 
-application class and discuss the results obtained with your instructor.
-*/
-
-
 
 class Mother 
 {
@@ -20,7 +10,7 @@ class Mother
 class Child extends Mother 
 {
     void show() {
-        System.out.println("Hello JUET");
+        System.out.println("Hello");
     }
 }
 
